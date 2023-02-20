9f1240a887920c1295d381d3f55c2f740e52d9cf
Phillip Ross <phillipross@users.noreply.github.com>
2023-02-20 18:39:35 -0500

    Multi module project setup test (#111)
    * rework testing directory to act as a multi-module project template
    
    Signed-off-by: Phillip Ross <phillip.w.g.ross@gmail.com>
    
    * add testing module to github actions workflow
    
    Signed-off-by: Phillip Ross <phillip.w.g.ross@gmail.com>
    
    ---------
    
    Signed-off-by: Phillip Ross <phillip.w.g.ross@gmail.com>
    (cherry picked from commit b0d33058d0528d020dd32efde776808a36dcf524)

--- testing/nio2-filewalker/src/main/java/co/luminositylabs/oss/testing/nio2/FileWalker.java
+++ testing/nio2-filewalker/src/main/java/co/luminositylabs/oss/testing/nio2/FileWalker.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 
 /**
- * A file visitor demonstrating minimal functionality of Java7+ NIO2 {@code Simple File Visitor}
+ * A file visitor demonstrating minimal functionality of Java7+ NIO2 {@code Simple File Visitor}.
  *
  * @author Phillip Ross
  */
